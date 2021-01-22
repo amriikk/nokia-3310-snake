35bfcaeea2af4ec2ed019a0f1fbfe22e0409c498
John Trujillo <amriikk@gmail.com>
2021-01-22 00:08:04 -0800

    implement eatApple function

--- app.js
+++ app.js
@@ -78,3 +78,17 @@ function checkForHits(squares){
             return false
     }
 }
+
+function eatApple(squares,tail){
+    if(squares[currentSnake[0]].classList.contains("apple")){
+        squares[currentSnake[0]].classList.remove("apple")
+        squares[tail].classList.add("snake")
+        currentSnake.push(tail)
+        randomApple(squares)
+        score++
+        scoreDisplay.textContent = score
+        clearInterval(interval)
+        intervalTime = intervalTime * speed
+        interval = setInterval(moveOutcome,intervalTime)
+    }
+}
